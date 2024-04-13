012152d12ecb6097ead58b6949a09b9d38ac1087
Gary Marsh <gmarsh207@caledonian.ac.uk>
2024-04-13 09:16:25 +0100

    added cusomisation of buttons using bootstrap

--- index.js
+++ index.js
@@ -18,10 +18,12 @@ app.set('view engine', 'mustache');
 const path = require('path');
 const public = path.join(__dirname,'public');
 app.use(express.static(public));
-
 const router = require('./routes/pantryRoutes');
 app.use('/', router); 
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000. Ctrl^c to quit.');
-})
+const port = process.env.PORT || 3000;
+app.listen(port, () =>
+ console.log(
+  `Express started on http://localhost:${port}` + "; press Ctrl-C to terminate."
+ )
+);
